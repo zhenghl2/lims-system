60cf3ad15acefa9a5d620045a4820fc1da045e46
LIMS Admin <admin@lims.local>
2026-07-29 14:27:57 +0800

    fix: 导入F用于Extraction destroy

--- lims/apps/cases/views.py
+++ lims/apps/cases/views.py
@@ -1,6 +1,6 @@
 """Case views."""
 from django.db import transaction
-from django.db.models import Q
+from django.db.models import Q, F
 from django.utils import timezone
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action, api_view, permission_classes
