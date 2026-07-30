27b83a254f14ea2c3c49b2b17aeb00f36f6bad49
LIMS Admin <admin@lims.local>
2026-07-30 16:11:04 +0800

    chore: remove debug file

--- check_hyb.py
+++
@@ -1,5 +0,0 @@
-import os,django;os.environ.setdefault('DJANGO_SETTINGS_MODULE','lims.config.settings.base');import django;django.setup()
-from lims.apps.cases.models import NipptHybSeqBatch
-for b in NipptHybSeqBatch.objects.all():
-    d = b.hyb_seq_data or {}
-    print(b.batch_number, 'mix_sources:', d.get('mix_sources','N/A'), 'chip:', d.get('chip_number','N/A'))
